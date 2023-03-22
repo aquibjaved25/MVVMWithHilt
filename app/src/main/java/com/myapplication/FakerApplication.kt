af5e0de4f10c15cddb0e7cc37b83d269c5592cf4
aquibjaved25 <aquib.javed25@gmail.com>
2023-03-22 18:22:26 +0530

    application class updated

--- app/src/main/java/com/myapplication/FakerApplication.kt
+++ app/src/main/java/com/myapplication/FakerApplication.kt
@@ -4,15 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class FakerApplication : Application() {
-
-
-    override fun onCreate() {
-        super.onCreate()
-
-
-
-    }
-
-
-}
+class FakerApplication : Application()
